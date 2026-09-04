285f53f906acc14e723e2cc21b8058065d96f4f1
Raj Bhokare <bhokareraj281@gmail.com>
2026-09-04 23:22:42 +0530

    Open-Meteo service layer

---
+++ src/services/weatherApi.js
@@ -0,0 +1,9 @@
+﻿import { fetchJson } from "../utils/fetchJson.js";
+
+const GEOCODING_BASE_URL = "https://geocoding-api.open-meteo.com/v1/search";
+const FORECAST_BASE_URL = "https://api.open-meteo.com/v1/forecast";
+
+// Suppress unused imports/variables warning until exported functions are added
+// oxlint-disable-next-line no-unused-vars
+const _dummy = { fetchJson, GEOCODING_BASE_URL, FORECAST_BASE_URL };
+
---
+++ src/utils/fetchJson.js
@@ -0,0 +1,8 @@
+﻿export async function fetchJson(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json();
+}
+
